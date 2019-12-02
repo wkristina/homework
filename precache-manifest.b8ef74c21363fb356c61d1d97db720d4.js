2a37a7d6cd74dfd77087c69ae3eb6001e637bf2f
wkristina <kristinawagner3@web.de>
2019-12-02 11:49:11 +0100

    Updates

---
+++ precache-manifest.b8ef74c21363fb356c61d1d97db720d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adcf534dfb885ba0125521be846824be",
+    "url": "/homework/index.html"
+  },
+  {
+    "revision": "267da7af701fb84d1de8",
+    "url": "/homework/static/css/2.247797c6.chunk.css"
+  },
+  {
+    "revision": "1f0fc9c091ebd9160bb0",
+    "url": "/homework/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "267da7af701fb84d1de8",
+    "url": "/homework/static/js/2.19087065.chunk.js"
+  },
+  {
+    "revision": "1f0fc9c091ebd9160bb0",
+    "url": "/homework/static/js/main.45eba45e.chunk.js"
+  },
+  {
+    "revision": "db14e617357b9f45f7c0",
+    "url": "/homework/static/js/runtime-main.d4cfbc0c.js"
+  }
+]);
